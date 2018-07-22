6befb8caea211562193d0103b62a0b4aacbfe6dd
Marco Biedermann <marco.biedermann@gmx.de>
2018-07-22 14:45:44 +0200

    add defaults and init method

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
+import * as d3 from 'd3';
+
 class D3LineChart {
   constructor(element, options) {
     const defaults = {
-
+      height: 370,
+      width: 500,
+      margin: {
+        bottom: 35,
+        left: 60,
+        right: 0,
+        top: 15,
+      },
     };
 
     this.element = element;
     Object.assign(this, defaults, options);
+
+    this.init();
+  }
+
+  init() {
+    const {
+      element, width, height, margin,
+    } = this;
+
+    this.svg = d3.select(element)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
   }
 
   render() {
