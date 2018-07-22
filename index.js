163fe6a1a07d6d7aba157587156a82b26fc3647e
Marco Biedermann <marco.biedermann@gmx.de>
2018-07-22 13:34:24 +0200

    setup base class

---
+++ index.js
@@ -0,0 +1,20 @@
+class D3LineChart {
+  constructor(element, options) {
+    const defaults = {
+
+    };
+
+    this.element = element;
+    Object.assign(this, defaults, options);
+  }
+
+  render() {
+
+  }
+
+  update() {
+    this.render();
+  }
+}
+
+export default D3LineChart;
