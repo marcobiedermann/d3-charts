0b6b8550d0fbfd64fce67320c6c06c2162b50fe9
Marco Biedermann <marco.biedermann@gmx.de>
2018-07-22 15:27:57 +0200

    render line

--- index.js
+++ index.js
@@ -19,10 +19,27 @@ class D3LineChart {
     this.init();
   }
 
+  dimensions() {
+    const {
+      margin,
+      width,
+      height,
+    } = this;
+
+    return [
+      width - margin.left - margin.right,
+      height - margin.top - margin.bottom,
+    ];
+  }
+
   init() {
     const {
-      element, width, height, margin,
+      element,
+      width,
+      height,
+      margin,
     } = this;
+    const [innerWidth, innerHeight] = this.dimensions();
 
     this.svg = d3.select(element)
       .append('svg')
@@ -30,19 +47,44 @@ class D3LineChart {
       .attr('height', height)
       .append('g')
       .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    this.svg.append('path')
+      .attr('class', 'd3-line-chart__line');
+
+    this.scaleX = d3.scaleTime()
+      .range([0, innerWidth]);
+
+    this.scaleY = d3.scaleLinear()
+      .range([innerHeight, 0]);
+
+    this.line = d3.line()
+      .curve(d3.curveBasis)
+      .x(data => this.scaleX(data.date))
+      .y(data => this.scaleY(data.value));
   }
 
   renderAxis() {
 
   }
 
-  renderLine() {
+  renderLine(data) {
+    const {
+      svg,
+      line,
+    } = this;
 
+    svg.select('.d3-line-chart__line')
+      .data([data])
+      .transition()
+      .attr('d', line);
   }
 
-  render() {
+  render(data) {
+    this.scaleX.domain(d3.extent(data, data => data.date));
+    this.scaleY.domain(d3.extent(data, data => data.value));
+
     this.renderAxis();
-    this.renderLine();
+    this.renderLine(data);
   }
 
   update() {
