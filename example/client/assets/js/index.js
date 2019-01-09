f84facc581a7848ebe314130657034f3ebac5293
Marco Biedermann <marco.biedermann@gmx.de>
2019-01-09 10:43:31 +0100

    docs: Example
    Remove example folder

--- example/client/assets/js/index.js
+++
@@ -1,7 +0,0 @@
-import D3LineChart from '../../../../index';
-import generateData from './generate-data';
-
-const d3LineChart = new D3LineChart('.js-d3-line-chart');
-const data = generateData(12);
-
-d3LineChart.render(data);
