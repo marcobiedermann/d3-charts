28ee87aec21f104bd0e87867a23786b6daeee161
Marco Biedermann <marco.biedermann@gmx.de>
2019-02-22 23:32:33 +0100

    docs: story
    add story

---
+++ packages/d3-line-chart/docs/stories.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import React, { Component } from 'react';
+import { storiesOf } from '@storybook/react';
+import D3LineChart from '..';
+
+function generateRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateData(n) {
+  const data = [];
+
+  while (n--) {
+    data.push([
+      new Date(Date.now() - (n * 1000 * 60 * 60 * 24)),
+      generateRandomInteger(0, 12),
+    ]);
+  }
+
+  return data;
+}
+
+class LineChart extends Component {
+  componentDidMount() {
+    this.chart = new D3LineChart(this.refs.chart);
+
+    this.chart.render(generateData(12));
+  }
+
+  render() {
+    return (
+      <div ref="chart" />
+    );
+  }
+}
+
+storiesOf('Line Chart', module)
+  .add('default', () => (
+    <LineChart />
+  ));
